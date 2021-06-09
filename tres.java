b256c0789ec11982e563bf97f0cdb0de836757a4
Byron Torres Ajxup <ajxupbyron@gmail.com>
2021-06-09 01:35:04 -0600

    tercero

---
+++ tres.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+public class tres {
+    public static void main(String[] args) {
+        int numero,doble,triple;
+        Scanner ent = new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        numero=ent.nextInt();
+        doble=numero*2;
+        triple=numero*3;
+        System.out.println("El doble es: "+doble);
+        System.out.println("El triple es: "+triple);
+
+    }
+    
+}
