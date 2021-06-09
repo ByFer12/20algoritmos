07b2da6027bfa76e4f572e576f00da4b220b3a77
Byron Torres Ajxup <ajxupbyron@gmail.com>
2021-06-09 02:37:19 -0600

    Septimo

---
+++ siete.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class siete {
+    public static void main(String[] args) {
+        int num1=0,num2,num3;
+        Random ale= new Random();
+        num1=(int)(Math.random()*51);
+        num2=(int)(num1-(num1*0.15));
+        System.out.println(num2);
+    }
+    
+}
